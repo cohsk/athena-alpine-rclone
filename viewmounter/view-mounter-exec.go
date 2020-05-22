ab3d75197c82c95565391a261470227ad85fa3a3
Steve Klosky <65673651+cohsk@users.noreply.github.com>
2020-05-21 18:00:58 -0700

    Update view-mounter-exec.go

--- viewmounter/view-mounter-exec.go
+++ viewmounter/view-mounter-exec.go
@@ -13,23 +13,21 @@
 package main
 
 import (
-
   "fmt"
   "os"
-  "bytes"
-  "encoding/json"
-  "io/ioutil"
-  "net/http"
-//  "bufio"
-//  "strconv"
-//  "strings"
-//  "time"
-)
+  CohesityManagementSdk "github.com/cohesity/management-sdk-go/managementsdk"
+  managementModels "github.com/cohesity/management-sdk-go/models"
+  )
 
-type authTokenRes struct {
-    AccessToken    string   `json:"accessToken"`
-    Privileges    []string   `json:"privileges"`
-    TokenType    string   `json:"tokenType"`
+// View Identification information.
+type ViewInfo struct {
+  ViewName string `json:"name,omitempty"`
+  ViewId   int    `json:"viewId,omitempty"`
+}
+
+// Represents the information of views.
+type ViewsInformation struct {
+  ViewsInfo []*ViewInfo `json:"views,omitempty"`
 }
 
 func PrintUsageAndExit() {
@@ -38,94 +36,46 @@ func PrintUsageAndExit() {
   os.Exit(0)
 }
 
-func GetAuthToken() string {
-
-  authToken := ""
-  endpointIp := ""
-  endpointPort := ""
-  username := ""
-  password := ""
-  domain := ""
-  var err error
-  var body []byte
-  var rContainer authTokenRes
-
-  _ = err
-  _ = body
-  _ = authToken
-
-  // gather user input
-  fmt.Println("Please enter ENDPOINT IP")
-  fmt.Scanln(&endpointIp)
-  fmt.Println("Please enter ENDPOINT PORT")
-  fmt.Scanln(&endpointPort)
-  fmt.Println("Please enter Username")
-  fmt.Scanln(&username)
-  fmt.Println("Please enter Password")
-  fmt.Scanln(&password)
-
-  // get auth token from cluster
-  tr := &http.Transport{
-    TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-  }
-  url := "https://" + endpointIp + ":" + endpointPort + "/irisservices/api/v1/public/accessTokens"
-  valuePair := map[string]string{"domain": domain, "password": password, "username": username}
-  postData, _ := json.Marshal(valuePair)
-  postReader := bytes.NewBuffer(postData)
-  client := &http.Client{Transport: tr}
-  req, _ := http.NewRequest("POST", url, postReader)
-  req.Header.Add("Accept", "application/json")
-  req.Header.Add("Content-type", "application/json")
-  res, _ := client.Do(req)
-  body, _ = ioutil.ReadAll(res.Body)
-  err = json.Unmarshal(body, &rContainer)
-  authToken = rContainer.AccessToken
-  fmt.Println(authToken)
-  defer res.Body.Close()
-  return authToken
-}
 
 func main() {
 
-  var body []byte
-  var authToken string
-
-  _ = authToken
-  _ = body
-  if len(os.Args) > 1  {
-    PrintUsageAndExit()
-  }
+  var viewsResult *managementModels.GetViewsResult
+  var viewNames, viewBoxNames, tenantIds []string
+  var matchPartialNames, includeInactive, allUnderHierarchy,
+    sortByLogicalUsage, matchAliasNames *bool
+  var maxCount, maxViewId *int64
+  var viewBoxIds, jobIds []int64
 
   fmt.Println(`Setting up Cohesity Access`)
+  Username := "admin"
+  Password := "admin"
+  ClusterVip := "172.16.3.101"
+  var Domain string // Set for AD user only.
+  client := CohesityManagementSdk.NewCohesitySdkClient(ClusterVip, Username, Password, Domain)
+  //CohesityManagementSdk.NewCohesityClientWithToken(hostIp, &managementAccessToken)
+  
+  fmt.Println(`Getting List of Views`)
+  viewsResult, _ = client.Views().GetViews(viewNames, viewBoxNames,
+    matchPartialNames, maxCount, maxViewId, includeInactive, tenantIds,
+    allUnderHierarchy, viewBoxIds, jobIds, sortByLogicalUsage, matchAliasNames)
+  clusterViews := viewsResult.Views
+  clusterViewIDMap := make(map[int]string)
 
-  // if we are in an athena setting, we look for these environment
-  // variables
-  //
-  // HOST_IP  # The Host IP on which the container is running.
-  // APPS_API_ENDPOINT_IP # Cohesity App Server IP.
-  // APPS_API_ENDPOINT_PORT # Cohesity App Server Port.
-  // APP_AUTHENTICATION_TOKEN # Authetication Token to make Cohesity App API calls. 
+  // ClusterViewsInfo gives information about all the views in a cluster.
+  // var clusterViewsInfo ViewsInformation
+  var viewsInfo []*ViewInfo
 
-  endpointIp := os.Getenv("APPS_API_ENDPOINT_IP")
-  endpointPort := os.Getenv("APPS_API_ENDPOINT_PORT")
-  authToken = os.Getenv("APP_AUTHENTICATION_TOKEN")
-
-  // Check to see if there are environment variables for login
-  // If not, prompt for access info
-  if (len(authToken) == 0) {
-    authToken = GetAuthToken()
+  // Iterating over cluster views and storing viewname and id in a map.
+  for _, view := range clusterViews {
+    clusterViewIDMap[int(*view.ViewId)] = *view.Name
+    viewInfo := ViewInfo{
+      ViewName: *view.Name,
+      ViewId:   int(*view.ViewId),
+    }
+    viewsInfo = append(viewsInfo, &viewInfo)
+    fmt.Println(view.Name)
   }
 
-  fmt.Println(`Getting List of Views`)
-
-  url := "https://" + endpointIp + ":" + endpointPort + "/irisservices/api/v1/public/views"
-  client := &http.Client{}
-  req, _ := http.NewRequest("GET", url, nil)
-  req.Header.Set("Authorization", "Bearer " + authToken)
-  req.Header.Add("Accept", "application/json")
-  res, _ := client.Do(req)
-  body, _ = ioutil.ReadAll(res.Body)
-  res.Body.Close()
-
   fmt.Println(`Mounting Views`)
+
 }
